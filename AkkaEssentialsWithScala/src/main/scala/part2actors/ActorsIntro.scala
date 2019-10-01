008d8647909d5c4ae2c9f3c97d801631eb46e884
Guisanpea <guisanpea@gmail.com>
2019-10-01 10:48:05 +0200

    Advanced a little

--- AkkaEssentialsWithScala/src/main/scala/part2actors/ActorsIntro.scala
+++ AkkaEssentialsWithScala/src/main/scala/part2actors/ActorsIntro.scala
@@ -1,8 +1,43 @@
 package part2actors
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorSystem, Props}
 
 object ActorsIntro extends App {
   val actorSystem = ActorSystem("firstActorSystem")
   println(actorSystem.name)
+
+  class WordCountActor extends Actor {
+    var totalWords = 0
+
+    def receive: PartialFunction[Any, Unit] = {
+      case message: String =>
+        println(s"[word counter] I have received: $message")
+        totalWords += message.split(" ").length
+      case msg => println(s"[word counter] I cannot understand ${msg.toString}")
+    }
+  }
+
+  val wordCounter = actorSystem.actorOf(Props[WordCountActor], "wordCounter")
+  val anotherWordCounter = actorSystem.actorOf(Props[WordCountActor], "anotherWordCounter")
+
+  wordCounter ! "I am learning Akka and it's pretty damn cool!"
+  anotherWordCounter ! 123
+
+  class Person(name: String) extends Actor {
+    override def receive: Receive = {
+      case "hi" => println(s"Hi my name is $name")
+      case _ =>
+    }
+  }
+
+  object Person {
+    def props(name: String) = Props(new Person(name))
+  }
+
+  // Bad practice
+  val person = actorSystem.actorOf(Props(new Person("Bob")))
+  person ! "hi"
+
+  // Good practice
+  val personNice = actorSystem.actorOf(Person.props("Bob"))
 }
